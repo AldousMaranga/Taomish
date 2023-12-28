89329f7b964d7ebe8b29419944b0c92c46183133
AldousMaranga <marangaaldous263@gmail.com>
2023-12-28 20:36:31 +0800

    job mobile view

--- src/Components/Jobs.js
+++ src/Components/Jobs.js
@@ -5,7 +5,7 @@ class Jobs extends Component {
     render() {
         return (
             <html>
-                <link rel="stylesheet" href="/assets/css/jobs.css" type="text/css"/>
+                <link rel="stylesheet" href="/assets/css/jobs.css" type="text/css" />
                 <div id="jobs">
                     <div className="min-height p-left d-none d-lg-block job-cont">
                         <div className="job-strip"></div>
@@ -29,13 +29,35 @@ class Jobs extends Component {
 
                         </div>
 
-                        <div className="job-strip1">
-                        </div>
+                        <div className="job-strip1"></div>
 
                         <div className="job-strip2"></div>
                         <div className="job-strip3"></div>
                     </div>
-                    <div className="min-height d-lg-none">Jobs</div>
+
+                    <div className="small-screen-cont d-lg-none">
+                        <div className="small-jobs-strip">
+                            <div className="title pb-3">JOIN US</div>
+                            <div className="job-text1 pb-2">
+                                Shape the future of technology
+                            </div>
+                            <div className="job-text2 pb-2">
+                                As a global company with unparalleled scale, a track record
+                                of pioneering innovation, and a huge and influential client
+                                base, we offer associates a chance to drive change and
+                                improve the lives of millions of people arount the world.
+                            </div>
+                            <hr className="pb-2"></hr>
+                            <h5 className="pb-2">India</h5>
+                            <h5 className="pb-2">Americas</h5>
+                            <h5 className="pb-2">Asia Pacific</h5>
+                            <h5 className="pb-2">Europe and UK</h5>
+                            <h5 className="pb-2">Middle East and Africa</h5>
+
+                            <div className="job-strip1-small"></div>
+
+                        </div>
+                    </div>
                 </div>
             </html>
         );
