27f811da0e78cb1dc093d65e585476a076e52af7
AldousMaranga <marangaaldous263@gmail.com>
2023-12-28 19:02:51 +0800

    work mobile view

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -41,7 +41,7 @@ class Work extends Component {
                                 </h6>
 
                                 <div>
-                                    <a href="#projects" style={{textDecoration: 'none', color: '#000000'}}>
+                                    <a href="#projects" style={{ textDecoration: 'none', color: '#000000' }}>
                                         <img src='assets/images/down-arrow.svg' alt='img' height='100px' width='auto' />
                                         <span >Projects</span>
                                     </a>
@@ -56,7 +56,41 @@ class Work extends Component {
                     </div>
                     <div className="small-screen-cont d-lg-none">
                         <div className="small-work-strip">
-                            
+                            <div className="title pt-5">WORK</div>
+                            <div className='worktext pt-3'>
+                                We manage all technical aspects of software product
+                                development includes design the software or daata
+                                architecture, perform the engineering – low code development,
+                                automation and systems integration – and manage technical
+                                operations
+                            </div>
+                            <h6 className="worklist">
+                                <img src='assets/images/arrow-right.svg' alt="img" />
+                                <span>Digital Consulting</span>
+                            </h6>
+                            <h6 className="worklist">
+                                <img src='assets/images/arrow-right.svg' alt="img" />
+                                <span>Product Engineering</span>
+                            </h6>
+                            <h6 className="worklist">
+                                <img src='assets/images/arrow-right.svg' alt="img" />
+                                <span>Vulnerability & Security consulting</span>
+                            </h6>
+                            <h6 className="worklist padding-bottom">
+                                <img src='assets/images/arrow-right.svg' alt="img" />
+                                <span>Reinforcement Data models and Analytics</span>
+                            </h6>
+
+                            <div>
+                                <a href="#projects" style={{ textDecoration: 'none', color: '#000000' }}>
+                                    <img src='assets/images/down-arrow.svg' alt='img' height='100px' width='auto' />
+                                    <span >Projects</span>
+                                </a>
+                            </div>
+
+                            <div className="globe-small">
+                                <img src='assets/images/globe.png' alt='img' className="align-bot" width='100%' height='auto'></img>
+                            </div>
                         </div>
                     </div>
                 </div>
