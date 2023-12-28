9d2e3aae7bf59dbdf2a5d6c9c8c6c31fd488488e
AldousMaranga <marangaaldous263@gmail.com>
2023-12-28 19:55:48 +0800

    mobile view projects and technology

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -25,7 +25,7 @@ class Projects extends Component {
                                                         </div>
                                                         <img src="assets/images/ask.svg" alt="img" width="auto" height="200px" />
                                                     </div>
-                                                    <div className="back-card" style={{paddingTop: '130px'}}>
+                                                    <div className="back-card" style={{ paddingTop: '130px' }}>
                                                         <div className="bc-txt1">Xceler.ai</div>
                                                         <div className="bc-txt2">Cloud first SaaS based contract management, planning till its fulfillment workflow driven platform. Modular and API driven for seamless data omni channel </div>
                                                     </div>
@@ -40,7 +40,7 @@ class Projects extends Component {
                                                         </div>
                                                         <img src="assets/images/wireframing.svg" alt="img" width="100%" height="auto" />
                                                     </div>
-                                                    <div className="back-card" style={{paddingTop: '130px'}}>
+                                                    <div className="back-card" style={{ paddingTop: '130px' }}>
                                                         <div className="bc-txt1">Xceler.ai</div>
                                                         <div className="bc-txt2">Comprehensive bank cash, credit facility and investment management platform providing a discerning view to the liquidity and risk.</div>
                                                     </div>
@@ -55,7 +55,7 @@ class Projects extends Component {
                                                         </div>
                                                         <img src="assets/images/security.svg" alt="img" width="100%" height="auto" />
                                                     </div>
-                                                    <div className="back-card" style={{paddingTop: '150px'}}>
+                                                    <div className="back-card" style={{ paddingTop: '150px' }}>
                                                         <div className="bc-txt1">Xceler.ai</div>
                                                         <div className="bc-txt2">Seamless inventory management platform. Workflow and driven build, draw, transfer and processing of inventory.</div>
                                                     </div>
@@ -70,7 +70,7 @@ class Projects extends Component {
                                                         </div>
                                                         <img src="assets/images/analytics.svg" alt="img" width="auto" height="220px" style={{ paddingLeft: '20px' }} />
                                                     </div>
-                                                    <div className="back-card" style={{paddingTop: '170px'}}>
+                                                    <div className="back-card" style={{ paddingTop: '170px' }}>
                                                         <div className="bc-txt1">Taru.ag</div>
                                                         <div className="bc-txt2">Agri value chain management platform to improve farm, farmer and farming livelihood</div>
                                                     </div>
@@ -86,7 +86,7 @@ class Projects extends Component {
 
                             <div className="project-strip2 padding-top">
                                 <a href="#technology" style={{ textDecoration: 'none', color: '#000000' }}>
-                                    <img src='assets/images/down-arrow.svg' alt='img' className="arrow-nav"/>
+                                    <img src='assets/images/down-arrow.svg' alt='img' className="arrow-nav" />
                                     <span>Technology</span>
                                 </a>
                             </div>
@@ -94,8 +94,89 @@ class Projects extends Component {
 
                     </div>
 
-                    <div className="min-height d-lg-none">
-                        Projects
+                    <div className="small-screen-cont d-lg-none">
+                        <div className="small-project-strip">
+                            <div className="project-cont-small ">
+                                <div className="title t-txt1-small">PROJECTS</div>
+                                <div className="t-txt2-small">Our Recent Adventures</div>
+                                <div className="t-txt3-small">
+                                    <div className="scroll-width">
+                                        <div className="card-column">
+                                            {/* Card 1 */}
+                                            <div className="card-cont">
+                                                <div className="card">
+                                                    <div className="front-card">
+                                                        <div className="card-text">
+                                                            Commodity Trading and Risk Management
+                                                        </div>
+                                                        <img src="assets/images/ask.svg" alt="img" width="auto" height="200px" />
+                                                    </div>
+                                                    <div className="back-card" style={{ paddingTop: '130px' }}>
+                                                        <div className="bc-txt1">Xceler.ai</div>
+                                                        <div className="bc-txt2">Cloud first SaaS based contract management, planning till its fulfillment workflow driven platform. Modular and API driven for seamless data omni channel </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {/* Card 2 */}
+                                            <div className="card-cont">
+                                                <div className="card">
+                                                    <div className="front-card">
+                                                        <div className="card-text pb-5">
+                                                            Treasury
+                                                        </div>
+                                                        <img src="assets/images/wireframing.svg" alt="img" width="100%" height="auto" />
+                                                    </div>
+                                                    <div className="back-card" style={{ paddingTop: '130px' }}>
+                                                        <div className="bc-txt1">Xceler.ai</div>
+                                                        <div className="bc-txt2">Comprehensive bank cash, credit facility and investment management platform providing a discerning view to the liquidity and risk.</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {/* Card 3 */}
+                                            <div className="card-cont">
+                                                <div className="card">
+                                                    <div className="front-card">
+                                                        <div className="card-text pb-4">
+                                                            Inventory Management
+                                                        </div>
+                                                        <img src="assets/images/security.svg" alt="img" width="100%" height="auto" />
+                                                    </div>
+                                                    <div className="back-card" style={{ paddingTop: '150px' }}>
+                                                        <div className="bc-txt1">Xceler.ai</div>
+                                                        <div className="bc-txt2">Seamless inventory management platform. Workflow and driven build, draw, transfer and processing of inventory.</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {/* Card 4 */}
+                                            <div className="card-cont">
+                                                <div className="card">
+                                                    <div className="front-card">
+                                                        <div className="card-text">
+                                                            Taru.ag
+                                                        </div>
+                                                        <img src="assets/images/analytics.svg" alt="img" width="auto" height="220px" style={{ paddingLeft: '20px' }} />
+                                                    </div>
+                                                    <div className="back-card" style={{ paddingTop: '170px' }}>
+                                                        <div className="bc-txt1">Taru.ag</div>
+                                                        <div className="bc-txt2">Agri value chain management platform to improve farm, farmer and farming livelihood</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        <div className="pt-5 small-project-strip pb-5">
+                            <a href="#technology" style={{ textDecoration: 'none', color: '#000000' }}>
+                                <img src='assets/images/down-arrow.svg' alt='img' className="arrow-nav" />
+                                <span>Technology</span>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
             </html>
--- src/Components/Technology.js
+++ src/Components/Technology.js
@@ -55,7 +55,52 @@ class Technology extends Component {
                         </div>
                     </div>
 
-                    <div className="min-height d-lg-none">Technology</div>
+                    <div className="small-screen-cont d-lg-none">
+                        <div className="small-technology-strip">
+                            <div className="title" style={{ paddingTop: "70px" }}>
+                                TECHNOLOGY
+                            </div>
+                            <div className="text-1-small">
+                                We are passionate technologist, problem solvers, envisioning the
+                                future of supply chain. The common vision to empowering our
+                                customers to create, secure, and operate adaptive applications
+                                that reduce costs, improve operations brought us as team
+                                together. We constantly endeavour to deliver a world class
+                                technology solution and digital transformation consulting
+                                services.
+                            </div>
+                            <div className="text-1-small">
+                                The diverse team comes from niche tech stack, functional and
+                                operations experts, data science to fulfil the common vision
+                            </div>
+                            <div className="pt-3 pb-3">
+                                <h6 className="arrow-text">
+                                    <img src='assets/images/arrow-right.svg' alt="img" />
+                                    <span>Cutting edge open source technology and certification</span>
+                                </h6>
+                                <h6 className="arrow-text">
+                                    <img src='assets/images/arrow-right.svg' alt="img" />
+                                    <span>Best in class UI and UX product experience</span>
+                                </h6>
+                                <h6 className="arrow-text">
+                                    <img src='assets/images/arrow-right.svg' alt="img" />
+                                    <span>Robust and Scalable backend supporting data and documents.</span>
+                                </h6>
+                                <h6 className="arrow-text">
+                                    <img src='assets/images/arrow-right.svg' alt="img" />
+                                    <span>Cutting edge CI/CD pipelines</span>
+                                </h6>
+                                <h6 className="arrow-text">
+                                    <img src='assets/images/arrow-right.svg' alt="img" />
+                                    <span>AI/ML and deployment of reinforcement learning</span>
+                                </h6>
+                            </div>
+                            <img src='assets/images/tech.jpeg' alt='img' width="100%" height="auto" className="pb-5"/>
+
+                        </div>
+                    </div>
+
+
                 </div>
             </html>
         );
