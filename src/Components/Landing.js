e8f6c696269236cf94a2451141ca8306feccc9a5
AldousMaranga <marangaaldous263@gmail.com>
2024-02-09 00:32:53 +0800

    added landing page images

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-// import 'react-responsive-carousel/lib/styles/carousel.min.css';
-// import { CCarousel, CCarouselItem } from '@coreui/react';
-// import '@coreui/coreui/dist/css/coreui.min.css'
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { CCarousel, CCarouselItem } from '@coreui/react';
+import '@coreui/coreui/dist/css/coreui.min.css'
 
 class Landing extends Component {
 
@@ -10,72 +10,58 @@ class Landing extends Component {
             <html>
                 <link rel="stylesheet" href="/assets/css/landing.css" type="text/css" />
                 <body>
-                    <div className="min-height" id="home">
-                        <div className="landing-cont">
-                            {/* <img src="assets/images/new-landing.png" alt="logo" className="under-image" /> */}
-                            <div className="over-text">
-                                <div className="text1">Bridging the complexity of SaaS cloud. Simplifying the digital transformation journey</div>
+                    <div id="home">
+                        <div className="min-height d-none d-lg-block">
+                            <div className="landing-cont">
+                                {/* <img src="assets/images/new-landing.png" alt="logo" className="under-image" /> */}
+                                <div className="landing-text">
+                                    <div className="over-text">
+                                        <div className="text1">Bridging the complexity of SaaS cloud. Simplifying the digital transformation journey</div>
+                                    </div>
+                                    <div className="over-text1">
+                                        <div className="text2">Passionate engineer and entrepreneur on quest to build better solution for tomorrow.</div>
+                                    </div>
+                                </div>
+
+                                <div className="landing-img-cont">
+                                    <CCarousel transition="crossfade">
+                                        <CCarouselItem>
+                                            <img src="assets/images/landing-img1.png" alt="img" className="landing-img" />
+                                        </CCarouselItem>
+                                        <CCarouselItem>
+                                            <img src="assets/images/landing-img2.png" alt="img" className="landing-img" />
+                                        </CCarouselItem>
+                                        <CCarouselItem>
+                                            <img src="assets/images/landing-img3.png" alt="img" className="landing-img" />
+                                        </CCarouselItem>
+                                    </CCarousel>
+                                </div>
                             </div>
-                            <div className="over-text1">
-                                <div className="text2">Passionate engineer and entrepreneur on quest to build better solution for tomorrow.</div>
+                        </div>
+
+                        <div className="landing-cont-small  d-lg-none">
+                            <div className="landing-text-small">
+                                <div className="over-text">
+                                    <div className="text1">Bridging the complexity of SaaS cloud. Simplifying the digital transformation journey</div>
+                                </div>
+                                <div className="over-text1">
+                                    <div className="text2">Passionate engineer and entrepreneur on quest to build better solution for tomorrow.</div>
+                                </div>
                             </div>
 
-                            {/* <div className="over-div">
-                            <CCarousel controls indicators transition="crossfade" className="custom-indicators">
-                                <CCarouselItem className="bg1">
-                                    <div>
-                                        <div className="row">
-                                            <div className="col-lg-4 d-none d-lg-block">
-                                                <img src='assets/images/bldg.png' alt="img" className="img-div"/>
-                                            </div>
-                                            <div className="col-lg-8  d-block " style={{ fontSize: '12px' }}>
-                                                In today’s mobile, ubiquitous, and instantaneous world. When other technology providers opt for easy problems, Taomish exclusively develops new age solutions for end-to-end supply chain, trading & risk where the structure of data models are much more complex & intense.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </CCarouselItem>
-                                <CCarouselItem className="bg2">
-                                    <div>
-                                        <div className="row">
-                                            <div className="col-lg-4 d-none d-lg-block">
-                                                <img src='assets/images/tesseract.png' alt="img" className="img-div"/>
-                                            </div>
-                                            <div className="col-lg-8  d-block" style={{ fontSize: '10px' }}>
-                                                <h6>What We Do</h6>
-                                                In today’s mobile, ubiquitous, and instantaneous world. When other technology providers opt for easy problems, Taomish exclusively develops new age solutions for end-to-end supply chain, trading & risk where the structure of data models are much more complex & intense.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </CCarouselItem>
-                                <CCarouselItem className="bg3">
-                                    <div>
-                                        <div className="row">
-                                            <div className="col-lg-4 d-none d-lg-block">
-                                                <img src='assets/images/bldg.png' alt="img" className="img-div"/>
-                                            </div>
-                                            <div className="col-lg-8 d-block" style={{ fontSize: '10px' }}>
-                                                <h6>Who We Are</h6>
-                                                In today’s mobile, ubiquitous, and instantaneous world. When other technology providers opt for easy problems, Taomish exclusively develops new age solutions for end-to-end supply chain, trading & risk where the structure of data models are much more complex & intense.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </CCarouselItem>
-                                <CCarouselItem className="bg4">
-                                    <div>
-                                        <div className="row">
-                                            <div className="col-lg-4 d-none d-lg-block">
-                                                <img src='assets/images/tesseract.png' alt="img" className="img-div"/>
-                                            </div>
-                                            <div className="col-lg-8  d-block" style={{ fontSize: '10px' }}>
-                                                <h6>Work With Us</h6>
-                                                In today’s mobile, ubiquitous, and instantaneous world. When other technology providers opt for easy problems, Taomish exclusively develops new age solutions for end-to-end supply chain, trading & risk where the structure of data models are much more complex & intense.
-                                            </div>
-                                        </div>
-                                    </div>
-                                </CCarouselItem>
-                            </CCarousel>
-                        </div> */}
-
+                            <div className="landing-img-cont-small">
+                                <CCarousel transition="crossfade" className="carousel">
+                                    <CCarouselItem>
+                                        <img src="assets/images/landing-img1.png" alt="img" className="landing-img" />
+                                    </CCarouselItem>
+                                    <CCarouselItem>
+                                        <img src="assets/images/landing-img2.png" alt="img" className="landing-img" />
+                                    </CCarouselItem>
+                                    <CCarouselItem>
+                                        <img src="assets/images/landing-img3.png" alt="img" className="landing-img" />
+                                    </CCarouselItem>
+                                </CCarousel>
+                            </div>
                         </div>
                     </div>
                 </body>
