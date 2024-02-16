5f2591c6ce0ce1d48bdffd09218adbf6bc031013
AldousMaranga <marangaaldous263@gmail.com>
2024-02-16 19:03:08 +0800

    technology content altered

--- src/Components/Technology.js
+++ src/Components/Technology.js
@@ -13,19 +13,8 @@ class Technology extends Component {
                                     TECHNOLOGY
                                 </div>
                                 <div className="text-1 tech-spacing">
-                                    We are passionate technologist, problem solvers, envisioning the
-                                    future of supply chain. The common vision to empowering our
-                                    customers to create, secure, and operate adaptive applications
-                                    that reduce costs, improve operations brought us as team
-                                    together. We constantly endeavour to deliver a world class
-                                    technology solution and digital transformation consulting
-                                    services.
+                                    We are a team of enthusiastic technologists and innovative problem-solvers, always looking ahead to the ever-evolving world of the supply chain. Our driving goal is to empower our clients by providing them with the tools and resources to build, protect, and optimize their operations through dynamic applications. With a diverse group of experts in technology, function, and operations, as well as data analysts, we are dedicated to delivering top-of-the-line solutions and consulting services that align with our collective vision for the future.
                                 </div>
-                                <div className="text-1 tech-spacing">
-                                    The diverse team comes from niche tech stack, functional and
-                                    operations experts, data science to fulfil the common vision
-                                </div>
-
 
                                 <div className="container-blue">
                                     {/* <img src='assets/images/tech.jpeg' alt='img' className="tech-img" /> */}
@@ -38,23 +27,23 @@ class Technology extends Component {
 
                                     <div className="text-2">
                                         <h6 className="arrow-text">
-                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                            <span style={{ fontWeight: 'bold' }}>Cutting edge open source technology and certification</span>
+                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                            <span style={{ fontWeight: 'bold' }}>Cutting-edge open-source technology and certification</span>
                                         </h6>
                                         <h6 className="arrow-text">
-                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                            <span style={{ fontWeight: 'bold' }}>Best in class UI and UX product experience</span>
+                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                            <span style={{ fontWeight: 'bold' }}>Best-in-class UI and UX product experience</span>
                                         </h6>
                                         <h6 className="arrow-text">
-                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                            <span style={{ fontWeight: 'bold' }}>Robust and Scalable backend supporting data and documents.</span>
+                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                            <span style={{ fontWeight: 'bold' }}>Robust and scalable backend supporting data and documents</span>
                                         </h6>
                                         <h6 className="arrow-text">
-                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                            <span style={{ fontWeight: 'bold' }}>Cutting edge CI/CD pipelines</span>
+                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                            <span style={{ fontWeight: 'bold' }}>Cutting-edge CI/CD pipelines</span>
                                         </h6>
                                         <h6 className="arrow-text">
-                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
+                                            <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
                                             <span style={{ fontWeight: 'bold' }}>AI/ML and deployment of reinforcement learning</span>
                                         </h6>
                                     </div>
@@ -74,39 +63,29 @@ class Technology extends Component {
                                     TECHNOLOGY
                                 </div>
                                 <div className="text-1-small">
-                                    We are passionate technologist, problem solvers, envisioning the
-                                    future of supply chain. The common vision to empowering our
-                                    customers to create, secure, and operate adaptive applications
-                                    that reduce costs, improve operations brought us as team
-                                    together. We constantly endeavour to deliver a world class
-                                    technology solution and digital transformation consulting
-                                    services.
-                                </div>
-                                <div className="text-1-small">
-                                    The diverse team comes from niche tech stack, functional and
-                                    operations experts, data science to fulfil the common vision
+                                    We are a team of enthusiastic technologists and innovative problem-solvers, always looking ahead to the ever-evolving world of the supply chain. Our driving goal is to empower our clients by providing them with the tools and resources to build, protect, and optimize their operations through dynamic applications. With a diverse group of experts in technology, function, and operations, as well as data analysts, we are dedicated to delivering top-of-the-line solutions and consulting services that align with our collective vision for the future.
                                 </div>
                                 <img src='assets/images/tech.jpeg' alt='img' width="100%" height="auto" className="pt-5" />
 
                                 <div className="container-blue-small">
                                     <h6 className="arrow-text">
-                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                        <span style={{ fontWeight: 'bold' }}>Cutting edge open source technology and certification</span>
+                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                        <span style={{ fontWeight: 'bold' }}>Cutting-edge open-source technology and certification</span>
                                     </h6>
                                     <h6 className="arrow-text">
-                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                        <span style={{ fontWeight: 'bold' }}>Best in class UI and UX product experience</span>
+                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                        <span style={{ fontWeight: 'bold' }}>Best-in-class UI and UX product experience</span>
                                     </h6>
                                     <h6 className="arrow-text">
-                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                        <span style={{ fontWeight: 'bold' }}>Robust and Scalable backend supporting data and documents.</span>
+                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                        <span style={{ fontWeight: 'bold' }}>Robust and scalable backend supporting data and documents</span>
                                     </h6>
                                     <h6 className="arrow-text">
-                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
-                                        <span style={{ fontWeight: 'bold' }}>Cutting edge CI/CD pipelines</span>
+                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
+                                        <span style={{ fontWeight: 'bold' }}>Cutting-edge CI/CD pipelines</span>
                                     </h6>
                                     <h6 className="arrow-text">
-                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow"/>
+                                        <img src='assets/images/arrow-right.svg' alt="img" className="arrow" />
                                         <span style={{ fontWeight: 'bold' }}>AI/ML and deployment of reinforcement learning</span>
                                     </h6>
                                 </div>
